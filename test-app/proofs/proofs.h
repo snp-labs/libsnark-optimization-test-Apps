3603dfbc6907edd323296eb80f76604d4065f046
Elden Park <eldeniyenden@gmail.com>
2023-02-16 16:28:24 +0900

    o

--- test-app/proofs/proofs.h
+++ test-app/proofs/proofs.h
@@ -1,1 +1,27 @@
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
 
+void hello_devworld(void);
+
+/**
+ * Add two signed integers.
+ *
+ * On a 64-bit system, arguments are 32 bit and return type is 64 bit.
+ */
+long long add_numbers(int x, int y);
+
+/**
+ * Take a zero-terminated C string and return its length as a
+ * machine-size integer.
+ */
+unsigned long string_length(const char *sz_msg);
+
+unsigned long leven(const char *s1, const char *s2);
+
+char *give_me_letter_a(unsigned long count);
+
+void countdown(void (*callback)(int));
+
+char *do_encryption(void);
--- test-app/test-app/ViewController.swift
+++ test-app/test-app/ViewController.swift
@@ -12,6 +12,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        hello_devworld()
+        let result = do_encryption()
+//             print("5 of letter A: \(five_a)")
+        if let five_a_cstr = result {
+            let five_a = String.init(cString: five_a_cstr)
+            print("result is: \(five_a)ms")
+        } else {
+            print("Returned string was null!")
+        }
+        
+//        let answer = add_numbers(15, 25)
+//        print("The result is: \(result)")
+//        let string = "for an anvil, this library sure is lightweight"
+//        print("The length is: \(string_length(string))")
+        
     }
 
 
